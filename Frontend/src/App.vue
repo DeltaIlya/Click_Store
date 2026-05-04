d01e1fd54a8d3d554172ad9e51c22effb5c262c2
xN3tr0Nx <168099813+xN3tr0Nx@users.noreply.github.com>
2026-05-04 15:13:28 +1000

    commit

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HomeView from './views/HomeView.vue'
+
+
 </script>
 
 <template>
-  <HomeView></HomeView>
+  <RouterView />
 </template>
 
 <style scoped>
-
+@import "tailwindcss";
 </style>
--- Frontend/src/router/index.ts
+++ Frontend/src/router/index.ts
@@ -1,23 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Catalog from '../views/Catalog.vue'
+import Cart from '../views/Cart.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'Catalog',
+      component: Catalog
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/cart',
+      name: 'Cart',
+      component: Cart
     },
-  ],
+    {
+      path: '/product/:id',
+      name: 'ProductDetail',
+      component: () => import('../views/ProductDetail.vue')
+    },
+  ]
 })
 
 export default router
