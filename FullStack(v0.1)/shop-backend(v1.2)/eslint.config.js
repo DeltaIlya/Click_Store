80865d26a84111bf30d4f0f8f9f486872c37b435
DeltaIlya <id1tarasov@gmail.com>
2026-05-21 01:35:49 +1000

    fixed: fix check linter and config file

--- FullStack(v0.1)/shop-backend(v1.2)/eslint.config.js
+++ FullStack(v0.1)/shop-backend(v1.2)/eslint.config.js
@@ -1,11 +1,18 @@
 import js from "@eslint/js";
+import globals from "globals";
 
 export default [
-    js.configs.recommended,
-    {
-        rules: {
-            "no-unused-vars": "warn",
-            "no-undef": "error"
-        }
-    }
+  js.configs.recommended,
+  {
+    languageOptions: {
+      // Это сообщает линтеру, что мы используем глобальные переменные Node.js
+      globals: {
+        ...globals.node, 
+      },
+    },
+    rules: {
+      // Здесь можно настроить правила, например, сделать console просто предупреждением:
+      "no-console": "off", 
+    },
+  },
 ];
